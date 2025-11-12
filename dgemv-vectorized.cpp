84c9189f85af2b38e11b7cd915e7dd2889cca4a5
temukaO <109631961+temukaO@users.noreply.github.com>
2025-11-11 18:03:44 -0800

    Update dgemv-vectorized.cpp

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,5 +8,11 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
-
+   for (int i = 0; i < n; i++) {
+      double sum = 0.0;
+      for (int j = 0; j < n; j++) {
+         sum += A[i * n + j] * x[j];
+      }
+      y[i] = y[i] + sum;
+   }
 }
